fa409ff3fbed0b5d18cda2ee2b94038bf16f94df
Vadim <ruban-vadim@mail.ru>
2021-02-21 16:23:10 +0300

    first commit

---
+++ ДЗ/6script.js
@@ -0,0 +1,21 @@
+'use strict'
+let number = 4.6
+console.log(-4 / 0)
+console.log('string' * 9)
+const persone = 'Alex'
+const bool = false
+
+let und
+console.log(und)
+
+const obj = {
+  name: 'Jhon',
+  age: 25,
+  isMarried: false,
+}
+
+// console.log(obj.name)
+console.log(obj["name"])
+                0         1      2   3       4  5
+let arr = ["plum.png","ttt.png",6,"eee.png",{},[]]
+console.log(arr[1]) 
---
+++ ДЗ/9script.js
@@ -0,0 +1,24 @@
+'use strict'
+// соединяем строку и объект
+console.log('arr' + ' - object')
+console.log(4 + '5')
+
+let incr = 10,
+  decr = 10
+
+// это инкремент декримент увеличивает и уменьшает на 1
+incr++
+decr--
+
+console.log(incr)
+console.log(decr)
+
+// разделить 5 на столько раз, на сколько возможно
+console.log(5 % 2)
+// сравнение
+console.log(4 * 2 == 8)
+// опаератоР И ИЛИ
+// && ||
+const isCheked = true
+isClose = true
+console.log(isCheked && isClose)
